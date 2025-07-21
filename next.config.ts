0d2b6f7c0e1c9cde3fbacf5b1771095b6efae3b5
Kenzoku00 <155788641+Kenzoku00@users.noreply.github.com>
2025-07-21 22:09:38 +0700

    Image Domain Example

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    domains: [
+      'drive.google.com',
+      'encrypted-tbn0.gstatic.com',
+    ],
+  },
 };
 
 export default nextConfig;
